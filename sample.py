77dda63550a881533879a6a186679c9a138f4255
rahibeast <101440371+rahibeast@users.noreply.github.com>
2022-03-12 03:26:18 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,13 @@
-def foo(a):
-  return a + 2
+"""
+Helper functions live here
+"""
+
+
+def increament_by_two(var_to_increment):
+    """
+    This function increments input by two
+    :param var_to_increment: param to increment
+    :return: incremented variable
+    """
+    return var_to_increment + 2
+  
